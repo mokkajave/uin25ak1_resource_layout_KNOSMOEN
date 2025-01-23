60d92a33c163bc0f493dbce34956393f6ea46961
simonakn <111729472+mokkajave@users.noreply.github.com>
2025-01-23 16:11:24 +0100

    completed.

--- js/script.js
+++ js/script.js
@@ -1,67 +1,42 @@
-//Button-variables
-const htmlBtn = document.getElementById("html-btn")
-const cssBtn = document.getElementById("css-btn")
-const jsBtn = document.getElementById("js-btn")
-const reactBtn = document.getElementById("react-btn")
-const sanityBtn = document.getElementById("sanity-btn")
+//Navigation bar
+let navbarHTML = ""
 
-//HTML
-htmlBtn.addEventListener("click", function() {
-    updateContent(0)
-    updateClassList(this)
-})
+resources.map((resource) => navbarHTML += `<li><button class="btn" onclick="updateContent('${resource.category}')">${resource.category}</button></li>`)
 
-//CSS
-cssBtn.addEventListener("click", function() {
-    updateContent(1)
-    updateClassList(this)
-})
+document.getElementById("nav-bar").innerHTML = navbarHTML
 
-//JavaScript
-jsBtn.addEventListener("click", function() {
-    updateContent(2)
-    updateClassList(this)
-})
-
-//React
-reactBtn.addEventListener("click", function() {
-    updateContent(3)
-    updateClassList(this)
-})
-
-//Sanity
-sanityBtn.addEventListener("click", function() {
-    updateContent(4)
-    updateClassList(this)
-})
-
-//Frontpage when loaded
-updateContent(0)
+//Frontpage when loaded, by default
+updateContent("HTML")
 
 //FUNCTIONS
-function updateContent(index) {
+function updateContent(categoryName) {
+    //Filters the resource by specified category name
+    const filteredCategory = resources.filter((resource) => resource.category === categoryName)[0]
+
     //Empties the variable for each click, so that it does'nt multiply
-    let sourcesHTML = ""
+    let resourcesHTML = ""
 
     //Updates "content" with information of a specified index
     document.getElementById("content").innerHTML = 
-            `<h2>${resources[index].category}</h2>
-            <p>${resources[index].text}</p>
-            <ul id="links"></ul>`
+            `<h2>${filteredCategory.category}</h2>
+            <p>${filteredCategory.text}</p>
+            <ul id="links">
+                ${filteredCategory.sources.map((source) => resourcesHTML += `<li><a href="${source.url}">${source.title}</a></li>`)}
+            </ul>`
 
-    //Maps through sources of a specified index and updates element "links"
-    resources[index].sources.map((source) => sourcesHTML += `<li><a href="${source.url}">${source.title}</a></li>`)
-    document.getElementById("links").innerHTML = sourcesHTML
-}
+    document.getElementById("links").innerHTML = resourcesHTML
 
-function updateClassList(button) {
-    //Removes class "white" from all buttons
-    htmlBtn.classList.remove("white")
-    cssBtn.classList.remove("white")
-    jsBtn.classList.remove("white")
-    reactBtn.classList.remove("white")
-    sanityBtn.classList.remove("white")
+    //Creating an iterable Collection of elements
+    const buttons = document.getElementsByClassName("btn")
 
-    //Adds class "white" to a specified button
-    button.classList.add("white")
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i]
+
+        if (button.innerHTML === categoryName) {
+            button.classList.add("white")
+        }
+        else {
+            button.classList.remove("white")
+        }
+    }
 }
